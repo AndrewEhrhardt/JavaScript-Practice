13ceabbf3f41e99691bc43a2608b3450bb7fc6b3
AndrewEhrhardt <49926696+AndrewEhrhardt@users.noreply.github.com>
2021-07-10 17:10:54 -0500

    Update javaScriptPractice.js

--- javaScriptPractice.js
+++ javaScriptPractice.js
@@ -1,3 +1,5 @@
+//JavaScript Practice Problems I've Completed from Edabit
+
 //return the sum of two numbers 
 function addition(a, b) {
 	return a+b;
@@ -7,3 +9,22 @@ function addition(a, b) {
 function concatName(firstName, lastName) {
 	return lastName + ", " + firstName;
 }
+
+//convert an int into a str
+function stringInt(str) {
+	return parseInt(str);
+}
+
+//if a/b devides evenly return true, else return false 
+function dividesEvenly(a, b) {
+	if (a%b === 0) {
+		return true;
+	} else return false;
+}
+
+//reverse an array
+function reverse(arr) {
+	return arr.reverse();
+}
+
+//
