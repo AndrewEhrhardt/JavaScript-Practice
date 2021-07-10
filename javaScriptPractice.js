cd5e22ed4b3d19df891fb4d325fe78db7a8df731
AndrewEhrhardt <49926696+AndrewEhrhardt@users.noreply.github.com>
2021-07-10 16:48:46 -0500

    Create javaScriptPractice.js

---
+++ javaScriptPractice.js
@@ -0,0 +1,9 @@
+//return the sum of two numbers 
+function addition(a, b) {
+	return a+b;
+}
+
+//Concatenate First and Last Name into One String
+function concatName(firstName, lastName) {
+	return lastName + ", " + firstName;
+}
